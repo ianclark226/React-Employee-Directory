0d412fe55946e09d6b57542b2664faad6f6c8d01
Ian Clark <ianclark226@gmail.com>
2020-04-06 16:01:05 -0400

    4/6

--- reacthw19/src/components/Table/table.js
+++ reacthw19/src/components/Table/table.js
@@ -1,24 +1,30 @@
-import React from 'react';
+import React from "react";
+
+import data from "../../data.json";
 
 export default function Table() {
-    return (
-        <table>
-            <thread>
-                <tr>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Phone</th>
-                    <th>Nationality</th>
-                </tr>
-            </thread>
-            <tbody>
-                <tr>
-                    <td>123</td>
-                    <td>zxc</td>
-                    <td>qwe</td>
-                    <td>asd</td>
-                </tr>
-            </tbody>
-        </table>
-    )
+  console.log(data);
+  return (
+    <table border={1}>
+      <thread>
+        <tr>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Phone</th>
+          <th>Nationality</th>
+        </tr>
+      </thread>
+      <tbody>
+        {data.map(({ name, email, phone, nat }) => (
+          <tr key={id.value}
+          >
+            <td>{`${name.first} ${name.last}`}</td>
+            <td>{email}</td>
+            <td>{phone}</td>
+            <td>{nat}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
 }
