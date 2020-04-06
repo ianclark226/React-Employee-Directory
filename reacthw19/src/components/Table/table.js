661b9185ce6bda3e738b2a76f2b6befda51cca0c
Ian Clark <ianclark226@gmail.com>
2020-04-06 15:21:46 -0400

    4/6

---
+++ reacthw19/src/components/Table/table.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default function Table() {
+    return (
+        <table>
+            <thread>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Nationality</th>
+                </tr>
+            </thread>
+            <tbody>
+                <tr>
+                    <td>123</td>
+                    <td>zxc</td>
+                    <td>qwe</td>
+                    <td>asd</td>
+                </tr>
+            </tbody>
+        </table>
+    )
+}
